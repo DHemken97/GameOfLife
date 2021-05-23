e4ea04fba952397399fb374a4631c70d64cd68e1
DHemken <DHemken@wardkraft.com>
2021-05-23 12:09:24 -0600

    Add project files.

---
+++ GameOfLifeSimulator/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace GameOfLifeSimulator
+{
+    public class Cell
+    {
+        public Cell[] neighbors;
+        public bool IsAlive;
+        public int Generation = 0;
+        public void Tick()
+        {
+            var living = neighbors.Count(cell => cell.IsAlive);
+            if (living == 3)
+                IsAlive = true;
+            else if (living < 2 || living > 3)
+                IsAlive = false;
+
+
+            Generation++;
+        }
+    }
+}
---
+++ GameOfLifeSimulator/Simulator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace GameOfLifeSimulator
+{
+    public static class Simulator
+    {
+
+        public static Cell[][] Board;
+        public static Random r = new Random();
+        public static void Randomize(int chance)
+        {
+            var size = 99;
+            Board = new Cell[size][];
+
+            for(var x=0; x<size; x++)
+            {
+                board[x] = new cell[size];
+
+                for (var y = 0; y< size; y++)
+                {
+                    Board[x][y] = new Cell { IsAlive = r.NextBool(chance) };
+                }
+            }
+        }
+        
+        public BitMap RenderBoard()
+        {
+
+        }
+
+        public static bool NextBool(this Random r, int truePercentage = 50)
+        {
+            return r.NextDouble() < truePercentage / 100.0;
+        }
+
+    }
+}
